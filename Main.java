b1ac9c60e721102405026e42cb10187c9765ae24
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-20 22:26:38 +0530

    Welcome to the Day8 Line Comparison Problem using OOPS Concept

---
+++ Main.java
@@ -0,0 +1,7 @@
+package day8oops.LineComparisonProblemusingOOPSConcept;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Day8 Line Comparison Problem using OOPS Concept");
+    }
+}
